e3b268aaf7a525b46b8d443df65285e41acab1f9
Lina <linabob1301@gmail.com>
2023-05-28 23:18:14 +0300

    Initial commit

---
+++ src/main/java/second/task/Main.java
@@ -0,0 +1,51 @@
+package second.task;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        List<User> listUser = new ArrayList<>();
+        try
+        {  BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\Lina\\IdeaProjects\\HomeWork10\\src\\main\\java\\second\\task\\file.txt"));
+            String headLine = in.readLine();
+            String lineText;
+            while ((lineText = in.readLine()) != null)
+            {
+                String[] line = lineText.split(" ");
+                listUser.add(new User(line[0],Integer.parseInt(line[1])));
+            }
+            for (User user:listUser)
+        {
+            System.out.println(user);
+        }
+            in.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("File read error");
+        }
+        writeUserListToJson(listUser, "C:\\Users\\Lina\\IdeaProjects\\HomeWork10\\src\\main\\java\\second\\task\\user.json");
+    }
+    private static void writeUserListToJson(List<User> listUser, String outputFile)
+    {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(listUser);
+        try (FileWriter fileWriter = new FileWriter("C:\\Users\\Lina\\IdeaProjects\\HomeWork10\\src\\main\\java\\second\\task\\user.json"))
+        {
+            fileWriter.write(json);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/third/task/Main.java
@@ -0,0 +1,76 @@
+package third.task;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Lina\\IdeaProjects\\HomeWork10\\src\\main\\java\\third\\task\\words.txt"));
+        try
+        {
+            String line = reader.readLine();
+            List<String> wordsList = new ArrayList<>();
+            while (line != null)
+            {
+                String[] words = line.split(" ");
+                for (int i = 0; i < words.length; i++)
+                {
+                    if(!words[i].equals(""))
+                    {
+                        wordsList.add(words[i]);
+                    }
+                }
+                line = reader.readLine();
+            }
+
+            HashMap<String, Integer> map = new HashMap<>();
+            for (String word : wordsList)
+            {
+                if(!map.containsKey(word))
+                {
+                    map.put(word, 1);
+                }
+                else
+                {
+                    map.put(word, map.get(word) + 1);
+                }
+            }
+
+            HashMap<Integer, Object> mapWithValuesOnlyAsKey = new HashMap<>();
+
+            for (Integer item: map.values())
+            {
+                mapWithValuesOnlyAsKey.put(item, null);
+            }
+
+            ArrayList<Integer> distinctValues = new ArrayList<>();
+
+            for (var item : mapWithValuesOnlyAsKey.keySet())
+            {
+                distinctValues.add(item);
+            }
+
+            Collections.sort(distinctValues, Collections.reverseOrder());
+
+            for (Integer item: distinctValues)
+            {
+                for (var mapItem: map.entrySet())
+                {
+                    if (mapItem.getValue().equals(item))
+                    {
+                        System.out.println(String.format("%1$s %2$s", mapItem.getKey(), mapItem.getValue()));
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
